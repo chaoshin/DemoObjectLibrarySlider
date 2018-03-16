fe4d3cf61ff134388aeeb5d6a8f869b1455e2369
Chao Shin <chao.shin@gmail.com>
2018-03-17 00:25:01 +0800

    Demo 簡易說明Xcode中Object Library的Slider

--- DemoObjectLibrarySlider/ViewController.swift
+++ DemoObjectLibrarySlider/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func mySliderAction(_ sender: UISlider) {
+                myImage.alpha = CGFloat(sender.value) // 利用Slider傳入的值，設定Image View的alpha
+    }
+    
+    
 }
 
